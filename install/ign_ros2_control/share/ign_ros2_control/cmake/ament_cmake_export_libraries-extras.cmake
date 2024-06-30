fa77f2befd2622966653c5e0103312275615a3bd
Rae <dingyanyan483@gmail.com>
2024-06-30 16:20:23 +0800

    Update

---
+++ install/ign_ros2_control/share/ign_ros2_control/cmake/ament_cmake_export_libraries-extras.cmake
@@ -0,0 +1,1 @@
+/home/rae/testbot_ws/build/ign_ros2_control/ament_cmake_export_libraries/ament_cmake_export_libraries-extras.cmake
--- src/testbot/launch/launch_sim.launch.py
+++ src/testbot/launch/launch_sim.launch.py
@@ -2,6 +2,8 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
@@ -10,6 +12,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch.actions import ExecuteProcess
+import xacro
 
 def generate_launch_description():
 
@@ -25,50 +28,69 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-
-    # # Declare the launch argument for the world file, 'empty.sdf' loaded at default.
-    # # Add world:=/path/to/desired/worldfile for manual world file selection.
-
-    
-
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    # gazebo = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory('ign_gazebo'), 'launch', 'gazebo.launch.py')]),
-    #          )
-
     gazebo = ExecuteProcess(
             cmd=['ign', 'gazebo','src/testbot/worlds/obstacle.sdf'],
             output='screen'
         )
-    
+
+
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description'],
                         output='screen')
 
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_cont","--controller-manager", "/controller_manager"],
-    )
 
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_broad","--controller-manager", "/controller_manager"],
-    )
+    # diff_drive_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["diff_cont","--controller-manager", "/controller_manager"],
+    # )
+
+    # joint_broad_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["joint_broad","--controller-manager", "/controller_manager"],
+    # )
     
+    diff_drive_spawner=ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'diff_cont'],
+        output='screen'
+    )
+        
+    joint_broad_spawner=ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_broad'],
+        output='screen'
+    )
 
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
+        output='screen'
+    )
 
     # Launch them all!
     return LaunchDescription([
-        rsp,
         
+        bridge,
+        rsp,
         # declare_world_cmd,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[joint_broad_spawner],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_broad_spawner,
+                on_exit=[diff_drive_spawner],
+            )
+        ),
 
     ])
