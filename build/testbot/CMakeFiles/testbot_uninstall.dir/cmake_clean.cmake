f59b97e73e04afbfe243ef99be45147ed12c6903
Rae <dingyanyan483@gmail.com>
2024-07-01 17:08:14 +0800

    Delete build directory

--- build/testbot/CMakeFiles/testbot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/testbot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/testbot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
